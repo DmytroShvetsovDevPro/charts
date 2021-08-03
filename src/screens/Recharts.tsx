20a283a3890d0d4acfbe9401c2c38ea1264500dc
DmytroShvetsovDevPro <dmytro.shvetsov@dev.pro>
2021-08-03 16:48:05 +0300

    edit examples of charts

--- src/screens/Recharts.tsx
+++ src/screens/Recharts.tsx
@@ -1,22 +1,43 @@
 import React, { FC } from 'react';
-import { LineChart, Line } from 'recharts';
+import { LineChart, Line, CartesianGrid, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
 
 const data = [
   { name: 'Page A', uv: 400 },
   { name: 'Page B', uv: 300 },
   { name: 'Page C', uv: 320 },
   { name: 'Page D', uv: 180 },
-  { name: 'Page E', uv: 550 },
+  { name: 'Page E', uv: 440 },
 ];
+
+const CustomizedDot: FC<any> = (props) => {
+  const { cx, cy } = props;
+
+  return (
+    <svg x={cx - 10} y={cy - 10} viewBox="0 0 100 100" width={20} height={20}>
+      <g>
+        <circle cx="50" cy="50" r="50" fill="#a8c6ff"/>
+        <circle cx="50" cy="50" r="30" fill="#0058ff" />
+      </g>
+    </svg>
+  );
+};
 
 export const Recharts: FC = () => {
   return (
     <div>
       <h1>Recharts 1</h1>
-      
-      <LineChart width={400} height={400} data={data}>
-        <Line type="monotone" dataKey="uv" stroke="#3c73a8" />
-      </LineChart>
+
+      <div style={{ maxWidth: 420, height: 300 }}>
+        <ResponsiveContainer width="100%" height="100%">
+          <LineChart width={600} height={300} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+            <Line type="monotone" dataKey="uv" stroke="#0058ff" dot={<CustomizedDot />} />
+            <CartesianGrid stroke="#dfdfdf" vertical={false} />
+            <XAxis dataKey="name" />
+            <YAxis orientation="right" padding={{ bottom: 10, top: 0 }} />
+            <Tooltip />
+          </LineChart>
+        </ResponsiveContainer>
+      </div>
     </div>
   );
 };
--- src/screens/Victory.tsx
+++ src/screens/Victory.tsx
@@ -1,9 +1,48 @@
-import React, { FC } from 'react';
+import { FC } from 'react';
+import { VictoryChart, VictoryLine, VictoryVoronoiContainer } from 'victory';
 
 export const Victory: FC = () => {
   return (
     <div>
       <h1>Victory 3</h1>
+
+      <div style={{ maxWidth: 400 }}>
+        <VictoryChart>
+          <VictoryLine
+            // labelComponent={
+            //   <VictoryTooltip
+            //     cornerRadius={({ datum }) => datum.x > 6 ? 0 : 20}
+            //     pointerLength={({ datum }) => datum.y > 0 ? 5 : 20}
+            //     flyoutStyle={{
+            //       stroke: ({ datum }) => datum.x === 10
+            //         ? "tomato"
+            //         : "black"
+            //     }}
+            //   />
+            // }
+            animate={{
+              duration: 1000,
+              onLoad: { duration: 1000 }
+            }}
+            style={{
+              data: { stroke: '#0058ff' },
+              parent: { border: '1px solid #ccc' },
+            }}
+            containerComponent={
+              <VictoryVoronoiContainer
+                labels={({ datum }) => `${datum.y}`}
+              />
+            }
+            data={[
+              { x: 1, y: 2, label: '2' },
+              { x: 2, y: 3, label: '3' },
+              { x: 3, y: 5, label: '5' },
+              { x: 4, y: 4, label: '4' },
+              { x: 5, y: 7, label: '7' },
+            ]}
+          />
+        </VictoryChart>
+      </div>
     </div>
-  )
-}
+  );
+};
